39a0d73ac2a77cbf6b8362d38cf47a23c580edbb
victor <victor@example.com>
2017-08-08 10:43:48 -0500

    first

---
+++ learning_templates/basic_app/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from . import views
+
+#Template tagging
+app_name = 'basic_app'
+
+urlpatterns=[
+    url(r'^relative/$',views.relative,name='relative'),
+    url(r'^other/$',views.other,name='other'),
+]
